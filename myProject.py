7185fe9ca030cfeffd5ea1a16cd82c4fb200799b
alejandro mota <aamotadev@gmail.com>
2022-10-12 23:21:33 -0500

    language code automata done

--- myProject.py
+++ myProject.py
@@ -212,10 +212,63 @@ def urlVar():
     )
     dfa2 = VisualDFA(dfa)
     print(dfa2.table) 
+def languageCod():
+    num_states = 18
+    statesLg = []
+    alphabet = ['L','A','N','G','U','E','C','O','D']
+    simbolos = [chr(95),chr(32),chr(45),chr(61),chr(39),chr(34)] 
+    
+    def createDict(list,value): 
+        myDict = dict()
+        for c in list:
+            myDict[c] = value
+        return myDict 
+        
+    for i in range(num_states):
+        state = 'q'+str(i)
+        statesLg.append(state)
+        
+    for c in string.ascii_lowercase:  
+        alphabet.append(c)
+
+    for i in range(num_states):
+        state = 'q'+str(i)
+        statesLg.append(state)
+        
+    dfa = DFA(
+        states = set(statesLg),
+        input_symbols=alphabet+simbolos,
+        transitions = {
+            'q0': {'L': 'q1'},
+            'q1': {'A': 'q2'},
+            'q2': {'N': 'q3'},
+            'q3': {'G': 'q4'},
+            'q4': {'U': 'q5'},
+            'q5': {'A': 'q6'},
+            'q6': {'G': 'q7'},
+            'q7': {'E': 'q8'},
+            'q8': {'_': 'q9'},
+            'q9': {'C': 'q10'},
+            'q10': {'O': 'q11'},
+            'q11': {'D': 'q12'},
+            'q12': {'E': 'q13'},
+            'q13': {' ': 'q13', '=': 'q14'},
+            'q14': {' ': 'q14', '"': 'q15', "'": 'q15'},  
+            'q15': dict({'-': 'q16'},**createDict(alphabet,'q15')),   
+            'q16': dict({'"': 'q17', "'": 'q17'},**createDict(alphabet,'q16')), 
+            'q17': {}
+        },
+        initial_state='q0',
+        final_states={'q17'},
+        allow_partial = True
+    )  
+    dfa2 = VisualDFA(dfa)
+    print(dfa2.table)
 def main():
     debugDfa()
     installedAppsDfa()  
     allowedHost() 
     urlVar()
+    languageCod()
 if __name__ == "__main__":
     main()
