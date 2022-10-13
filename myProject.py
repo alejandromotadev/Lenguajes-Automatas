069bc01c2a837342028ab98db109662828293c51
alejandro mota <aamotadev@gmail.com>
2022-10-12 21:38:22 -0500

    BUG SOLVED

--- myProject.py
+++ myProject.py
@@ -40,7 +40,7 @@ def installedAppsDfa():
     num_states = 20
     states_iA = []
     alphabet = []
-    simbolos = [chr(32),chr(34),chr(39),chr(46),chr(44),chr(61),chr(91),chr(93),chr(95)] #space, "", ', ',', =, [, ],_ 
+    symbols = [chr(32),chr(34),chr(39),chr(46),chr(44),chr(61),chr(91),chr(93),chr(95)] #space, "", ', ',', =, [, ],_ 
     
     def createDict(list,value): 
         myDict = dict()
@@ -57,7 +57,7 @@ def installedAppsDfa():
         
     dfa = DFA(
         states = set(states_iA),
-        input_symbols=alphabet+simbolos,
+        input_symbols=alphabet+symbols,
         transitions = {
             'q0': {'I': 'q1'},
             'q1': {'N': 'q2'},
@@ -89,7 +89,7 @@ def allowedHost():
     num_states = 23
     statesaH = []
     alphabet = []
-    simbolos = [chr(48),chr(49),chr(50),chr(51),chr(52),chr(53),chr(54),chr(55),chr(56),chr(57),chr(32),chr(34),chr(39),chr(42),chr(46),chr(44),chr(61),chr(91),chr(93),chr(95)] #0,1,2,3,4,5,6,7,8,9 space, "", ', ',', =, [, ],_,* 
+    symbols = [chr(48),chr(49),chr(50),chr(51),chr(52),chr(53),chr(54),chr(55),chr(56),chr(57),chr(32),chr(34),chr(39),chr(42),chr(46),chr(44),chr(61),chr(91),chr(93),chr(95)] #0,1,2,3,4,5,6,7,8,9 space, "", ', ',', =, [, ],_,* 
     def createDict(list,value): 
         myDict = dict()
         for c in list:
@@ -105,7 +105,7 @@ def allowedHost():
         
     dfa = DFA(
         states = set(statesaH),
-        input_symbols=alphabet+simbolos,
+        input_symbols=alphabet+symbols,
         transitions = {
             'q0': {'A': 'q1'},
             'q1': {'L': 'q2'},
@@ -123,11 +123,11 @@ def allowedHost():
             'q13': {'=': 'q14', ' ': 'q13'},
             'q14': {'[': 'q15'},
             'q15': {'"': 'q16', "'": 'q16', ' ': 'q15', '*': 'q19', ']': 'q22'},
-            'q16': dict({'0': 'q20','1': 'q20','2': 'q20','3': 'q20','4': 'q20','5': 'q20','6': 'q20','7': 'q20','8': 'q20','9': 'q20'},**createDict(alphabet, 'q17')),
+            'q16': dict({'0': 'q20','1': 'q20','2': 'q20','3': 'q20','4': 'q20','5': 'q20','6': 'q20','7': 'q20','8': 'q20','9': 'q20'},**createDict(alphabet,'q17')),
             'q17': {'.': 'q18'},
-            'q18': dict({'.': 'q18', '"': 'q19', "'": 'q19'},**createDict(alphabet, 'q18')),
+            'q18': dict({'.': 'q18', '"': 'q19', "'": 'q19'},**createDict(alphabet,'q18')),
             'q19': {']': 'q22'},
-            'q20': {'.', 'q21'},
+            'q20': {'.': 'q21'},
             'q21': {'"': 'q19',"'": 'q19', '.': 'q21','0': 'q21','1': 'q21','2': 'q21','3': 'q21','4': 'q21','5': 'q21','6': 'q21','7': 'q21','8': 'q21','9': 'q21'}   
         },
         initial_state='q0',
