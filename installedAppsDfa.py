74a6c047ebc67a813d066f377753ba951df8d4c6
alejandro mota <aamotadev@gmail.com>
2022-10-14 13:10:54 -0500

    its workiiing

--- installedAppsDfa.py
+++ installedAppsDfa.py
@@ -17,33 +17,35 @@ for i in range(num_states):
     
 for c in string.ascii_letters:  
     alphabet.append(c)
-    
-dfaIA = DFA(
-    states = set(states_iA),
-    input_symbols=alphabet+symbols,
-    transitions = {
-        'q0': {'I': 'q1'},
-        'q1': {'N': 'q2'},
-        'q2': {'S': 'q3'},
-        'q3': {'T': 'q4'},
-        'q4': {'A': 'q5'},
-        'q5': {'L': 'q6'},
-        'q6': {'L': 'q7'},
-        'q7': {'E': 'q8'},
-        'q8': {'D': 'q9'},
-        'q9': {'_': 'q10'},
-        'q10': {'A': 'q11'},
-        'q11': {'P': 'q12'},
-        'q12': {'P': 'q13'},
-        'q13': {'S': 'q14'},
-        'q14': {'=': 'q15', ' ': 'q14'},
-        'q15': {'[': 'q16', ' ': 'q15'},
-        'q16': {' ': 'q16', "'": 'q17','"': 'q17'},
-        'q17': dict({'.': 'q17', '_': 'q17', "'": 'q18', '"': 'q18'},**createDict(alphabet,'q17')),
-        'q18': {' ': 'q18', ',': 'q16', ']': 'q19'}
-        
-    },
-    initial_state='q0',
-    final_states={'q19'},
-    allow_partial=True, 
-)
+
+def declaracionDFA():  
+    dfaIA = DFA(
+        states = set(states_iA),
+        input_symbols=alphabet+symbols,
+        transitions = {
+            'q0': {'I': 'q1'},
+            'q1': {'N': 'q2'},
+            'q2': {'S': 'q3'},
+            'q3': {'T': 'q4'},
+            'q4': {'A': 'q5'},
+            'q5': {'L': 'q6'},
+            'q6': {'L': 'q7'},
+            'q7': {'E': 'q8'},
+            'q8': {'D': 'q9'},
+            'q9': {'_': 'q10'},
+            'q10': {'A': 'q11'},
+            'q11': {'P': 'q12'},
+            'q12': {'P': 'q13'},
+            'q13': {'S': 'q14'},
+            'q14': {'=': 'q15', ' ': 'q14'},
+            'q15': {'[': 'q16', ' ': 'q15'},
+            'q16': {' ': 'q16', "'": 'q17','"': 'q17'},
+            'q17': dict({'.': 'q17', '_': 'q17', "'": 'q18', '"': 'q18'},**createDict(alphabet,'q17')),
+            'q18': {' ': 'q18', ',': 'q16', ']': 'q19'}
+            
+        },
+        initial_state='q0',
+        final_states={'q19'},
+        allow_partial=True, 
+    )
+    return dfaIA
--- myProject.py
+++ myProject.py
@@ -3,78 +3,115 @@ import installedAppsDfa as dfaIAPPS
 import allowedH as dfaAH
 import urlVarDfa as dfaUrl
 import langDfa as dfaLanguage
-import databasesDfa as dfaDB
+from databasesDfa import declaracionDFA as dfaDB
+from installedAppsDfa import declaracionDFA as dfaIA
    
 def leerDebug():
+    print('debuig leer')
     with open('./ejemplos/ejemplo 1.txt', 'r') as infile:
             data = infile.readline()
+            print(data.strip())
             while data:     
                 if dfaDebug.dfaDbg.accepts_input(data.strip()):
-                    print('aceptado\n')
+                    print('aceptado\t')
                     print(data.strip())
                     break
                 else:
                     print('rechazado')
                 data=infile.readline()
             print(data)
-def leerInstalledApps():
+            
+def leerInstalledApps():   
+    print('installed apps leer')
+    installedApps = ""
     with open('./ejemplos/ejemplo 1.txt', 'r') as infile:
-            data = infile.readline()
-            while data:     
-                if dfaIAPPS.dfaIA.accepts_input(data.strip()):
-                    print('aceptado\n')
-                    print(data.strip())
-                    break
-                else:
-                    print('rechazado')
-                data=infile.readline()
-            print(data)
+            data = infile.readlines()
+            for element in data:
+                data[data.index(element)] = element.rstrip() #limpia las lineas
+            for linea in data[data.index('INSTALLED_APPS = ['):data.index(']')+1]: #concatena en una linea
+                installedApps += linea
+            if dfaIA().accepts_input(installedApps):
+                print('aceptado\t') 
+            else:
+                print('rechazado')
+                print(installedApps)
+                dfaDB().read_input(installedApps)
+            for linea in data[data.index('INSTALLED_APPS = ['):data.index(']')+1]: #concatena en una linea
+                print(linea)
+    # with open('./ejemplos/ejemplo 1.txt', 'r') as infile:
+    #         data = infile.readline()
+    #         print(data.strip())
+    #         while data:     
+    #             if dfaIAPPS.dfaIA.accepts_input(data.rstrip()):
+    #                 print('aceptado\t')
+    #                 # print(data.rstrip())
+    #                 break
+    #             else:
+    #                 print('rechazado')
+    #             data=infile.readline()
+    #         print(data)
+            
 def leerAh():
+    print('allowes hosts leer')
     with open('./ejemplos/ejemplo 1.txt', 'r') as infile:
             data = infile.readline()
+            print(data.strip())
             while data:     
                 if dfaAH.dfaAh.accepts_input(data.strip()):
-                    print('aceptado\n')
+                    print('aceptado\t')
                     print(data.strip())
                     break
                 else:
                     print('rechazado')
+                    
                 data=infile.readline()
             print(data)
+            
 def leerUrl():
+    ('url variables leer')
     with open('./ejemplos/ejemplo 1.txt', 'r') as infile:
             data = infile.readline()
+            print(data.strip())
             while data:     
                 if dfaUrl.dfaURL.accepts_input(data.strip()):
-                    print('aceptado\n')
+                    print('aceptado\t')
                     print(data.strip())
                 else:
-                    print('rechazado')
+                   print('rechazado')
                 data=infile.readline()
             print(data)
+            
 def leerLang():
+    print('language leer')
     with open('./ejemplos/ejemplo 1.txt', 'r') as infile:
             data = infile.readline()
             while data:     
                 if dfaLanguage.dfaLg.accepts_input(data.strip()):
-                    print('aceptado\n')
+                    print('aceptado\t')
                     print(data.strip())
                     break
                 else:
                     print('rechazado')
                 data=infile.readline()
             print(data)
+            
 def leerDatabase():
+    print('Base de datos leer')
+    databases = ""
     with open('./ejemplos/ejemplo 1.txt', 'r') as infile:
-            data = infile.readline()
-            while data:     
-                if dfaDB.dfaDb.accepts_input(data.strip()):
-                    print('aceptado\n')
-                    print(data.strip())
-                else:
-                    print('rechazado')
-                data=infile.readline()
-            print(data)
+            data = infile.readlines()
+            for element in data:
+                data[data.index(element)] = element.rstrip() #limpia las lineas
+            for linea in data[data.index('DATABASES = {'):data.index('}')+1]: #concatena en una linea
+                databases += linea
+            if dfaDB().accepts_input(databases):
+                print('aceptado\t') 
+            else:
+                print('rechazado')
+                print(databases)
+                dfaDB().read_input(databases)
+            for linea in data[data.index('DATABASES = {'):data.index('}')+1]: #concatena en una linea
+                print(linea)
         
 def main():
     leerDebug()
@@ -85,4 +122,3 @@ def main():
     leerDatabase()
 if __name__ == "__main__":
     main()
-    
