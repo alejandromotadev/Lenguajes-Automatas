bd8379e1ff0d1530cdbe99a8ec875a3c324fb475
alejandro mota <aamotadev@gmail.com>
2022-10-14 20:52:09 -0500

    done

--- interfaz.py
+++ interfaz.py
@@ -9,6 +9,7 @@ window.geometry('200x200')
 ruta_str = "Ruta: "
 def abrir_archivo():
     file = filedialog.askopenfilename(title="Abrir", initialdir="./", filetypes=(("Archivos .txt", "*.txt"), ("Archivos .py", "*.py")))
+    # variables = []
     if file is None:
         return
     fileDFAdebug = leerDebug(file)
@@ -22,12 +23,20 @@ def abrir_archivo():
         os.remove(name+'.txt')
     else:
         with open('./reportes/'+name+'.txt', 'w') as file:
-            file.write(fileDFAdebug)
-            file.write(fileDFAia)
-            file.write(fileDFAah)
-            file.write(fileDFAurl)
-            file.write(fileDFAlang)
-            file.write(fileDFAdb)
+            file.write('-------------------------------------------------------LA CONSTRUCCION DEL PROYECTO--------------------------------------------------------------\n')
+            file.write('-------------------------------------------------------------------------------------------------------------------------------------------------\n')
+            file.write(f'EL DEBUG ESTA SENTENCIADO DE LA SIGUIENTE FORMA: {fileDFAdebug}\n')
+            file.write('-------------------------------------------------------------------------------------------------------------------------------------------------\n')
+            file.write(f'LOS COMPONENTES Y MODULOS DEL PROYECTO: {fileDFAia}\n')
+            file.write('-------------------------------------------------------------------------------------------------------------------------------------------------\n')
+            file.write(f'LOS HOSTS PERIMITDOS: {fileDFAah}\n')
+            file.write('-------------------------------------------------------------------------------------------------------------------------------------------------\n')
+            file.write(f'LAS VARIABLES DE URL: {fileDFAurl}\n')
+            file.write('-------------------------------------------------------------------------------------------------------------------------------------------------\n')
+            file.write(f'EL LANGUAGE CODE: {fileDFAlang}\n')
+            file.write('-------------------------------------------------------------------------------------------------------------------------------------------------\n')
+            file.write(f'LA BASES DE DATOS: {fileDFAdb}\n')
+            file.write('-------------------------------------------------------------------------------------------------------------------------------------------------\n')
 button = Button(text='Abrir',command=abrir_archivo)
 button.pack()
 lblNombreArchivo=Label(text="Nombre del archivo: ").pack()
